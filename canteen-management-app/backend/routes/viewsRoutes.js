63b7199570359c7a093de2c8bdd689f5068fb57c
Amar Singh <gujraal2006@gmail.com>
2025-06-09 01:47:52 +0530

    Added all students tab

--- canteen-management-app/backend/routes/viewsRoutes.js
+++ canteen-management-app/backend/routes/viewsRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const PredefinedPlans = require('../models/PredefinedPlans');
+const Student = require('../models/Student');
 const Ledger = require('../models/Ledger');
 const router = express.Router();
 // const { testPrint, printReceipt, printReceiptWithLogo } = require('../utils/printerHandler');
@@ -78,6 +79,16 @@ router.get('/students/add-student', (req, res, next) => {
 router.get('/add-staff', (req, res, next) => {
   try {
     res.render('dashboard/add-staff');
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Total Students
+router.get('/students/total-students',async (req, res, next) => {
+  try {
+        let students = await Student.find().populate('currentPlan');
+    res.render('dashboard/total-students',{students});
   } catch (err) {
     next(err);
   }
